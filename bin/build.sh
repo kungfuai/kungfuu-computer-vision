b28a84c1521e353a8decd6f5266423b200e5c0d9
ZZ Si <zhangzhang.si@gmail.com>
2022-07-12 23:55:15 -0500

    add an index page to link to courses

--- bin/build.sh
+++ bin/build.sh
@@ -1,1 +1,5 @@
-jb build $@
+make html
+course=course1-image-classification
+jb build $course
+mv $course/_build/html _build/html/$course
+
---
+++ conf.py
@@ -0,0 +1,4 @@
+extensions = ["myst_parser"]
+templates_path = ["_templates"]
+html_sidebars = {"**": []}
+exclude_patterns = ["_build", "README.md", "course1-image-classification"]
