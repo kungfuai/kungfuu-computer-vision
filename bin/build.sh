b4cb6ca1693ebd71b979ce52a1ad51e58da538cc
ZZ Si <zhangzhang.si@gmail.com>
2022-08-19 22:09:00 -0500

    more info in ci build

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,10 @@
 make html
 course=course1-image-classification
 jb build $course # --clear-output
+if test -f "$course/_build/html/index.html"; then
+    echo "Build success"
+else
+    echo "Build failed for $course"
+    exit 1
+fi
 mv $course/_build/html _build/html/$course
-
